46e88ddae090adcd8aac51b26e9a082a07aecbb0
SarcasticeSardines <a.hillstrom311@gmail.com>
2024-02-12 14:17:17 -0800

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+
+
+let addBtn = document.getElementById("addBtn");
+let toDoCount = document.getElementById("toDoCount");
+let inProgCount = document.getElementById("inProgCount");
+let completeCount = document.getElementById("completeCount");
+let todos = document.getElementById("todos");
+let inprogs = document.getElementById("inprogs");
+let completes = document.getElementById("completes");
+let projName = document.getElementById("projName");
+let projDesc = document.getElementById("projDesc");
+let choiceA = document.getElementById("choiceA");
+let choiceB = document.getElementById("choiceB");
+let choiceC = document.getElementById("choiceC");
+let projDate = document.getElementById("projDate");
+let saveit = document.getElementById("saveit");
+
+let task = "";
+let priority = 0;
+//1 is to-do, 2 is in progress, 3 is complete
+
+addBtn.addEventListener("click", (e)=>{
+        
+});
+
+saveit.addEventListener("click", ()=>{
+    // savetolocalstorage(task[0]name, desc, priority, due date)
+    //task divs- 1=todos.append(div), 2=inprogs.append(div), 3=completes.append(div)
+});
